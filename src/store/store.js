506050c67146aafc4e45afd22cf482ffeb7c9c3c
Utkarsh <utkarshchoudhary405@gmail.com>
2025-04-19 12:27:22 +0530

    configured redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './Slice/counterSlice'
-import loginReducer from './Slice/loginSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./Slice/counterSlice";
+import loginReducer from "./Slice/loginSlice";
+import productSaga from "./saga/productSaga";
+import createSagaMiddleware from "redux-saga";
+import React from "react";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
-    counter:counterReducer,
-    login:loginReducer
+    counter: counterReducer,
+    login: loginReducer,
   },
-})
+  middleware: () => [sagaMiddleware],
+});
+
+sagaMiddleware.run(productSaga)
