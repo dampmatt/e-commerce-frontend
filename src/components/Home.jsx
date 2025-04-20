0970fb8949e1dddce9bf2c5e57bef94163190ea1
Utkarsh <utkarshchoudhary405@gmail.com>
2025-04-21 01:26:21 +0530

    configured increment and decrement reducers and added 4 images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router'
 
 const Home = () => {
   const products = useSelector((state) => state.products.items)
@@ -45,8 +46,10 @@ const Home = () => {
                         <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                     </div>
                     <div class="flex items-center justify-between">
-                        <span class="text-3xl font-bold text-gray-900 dark:text-white">{item.price}</span>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        <span class="text-3xl font-bold text-gray-900 dark:text-white">${item.price}</span>
+                        <button 
+                        type='button'
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">Add to cart</button>
                     </div>
                 </div>
             </div>
--- src/store/Slice/counterSlice.js
+++ src/store/Slice/counterSlice.js
@@ -1,26 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: 0,
+  cartList:[]
 }
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1
+    increment: (state, action) => {
+      const item = state.cartList.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.cartList.push({ ...action.payload, quantity: 1 });
+      }
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value = action.payload
+    decrement: (state, action) => {
+      const itemIndex = state.cartList.findIndex(i => i.id === action.payload);
+      if (itemIndex !== -1) {
+        const item = state.cartList[itemIndex];
+        if (item.quantity > 1) {
+          item.quantity -= 1;
+        } else {
+          state.cartList.splice(itemIndex, 1); 
+        }
+      }
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement } = counterSlice.actions
 
 export default counterSlice.reducer
