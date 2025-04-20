784bd8cbccde08e7adcabe105d30b9e0730617f7
Utkarsh <utkarshchoudhary405@gmail.com>
2025-04-21 02:45:26 +0530

    done ith header login and shit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom' // Correct import for react-router-dom
 const Home = () => {
   const products = useSelector((state) => state.products.items)
   const loading = useSelector((state) => state.products.loading)
-
+  const isLoggedIn = useSelector((state)=>state.login.login)
   console.log('products:', products)
 
   return (
@@ -44,7 +44,13 @@ const Home = () => {
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">${item.price}</span>
                 <button 
                   type='button'
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">Add to cart</button>
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                  disabled= {isLoggedIn}
+                  >
+                    Add to cart
+                </button>
+
+                  
               </div>
             </div>
           </div>
