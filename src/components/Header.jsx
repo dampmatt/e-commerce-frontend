e9db9d77069493b756a41b52ea5a2ccf0db3732c
Utkarsh <utkarshchoudhary405@gmail.com>
2025-04-21 03:29:07 +0530

    added some reactivity to links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,27 @@ const Header = () => {
   const dispatch = useDispatch()
   const navigate= useNavigate();
 
+  const handleLinkClick = ()=>{
+    navigate('/')
+  }
+
   const handleClick = ()=>{
     if(!isLogin){
       dispatch(SignIn());
     }
     else{
       navigate('/cart');
-    }
-    
-    
+    }    
   }
   return (
     <div className='flex justify-between items-center bg-[#1a1a2e] text-pink-500  px-8  '>
-        <h1 className='text-3xl font-bold cursor-pointer py-6'>LOGO</h1>
+        <h1 className='text-3xl font-bold cursor-pointer py-6' onClick={handleLinkClick}>LOGO</h1>
 
-        <ul className='flex  font-thin self-baseline-last '>    
-            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm'>Electronics</li>
-            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm'>Clothes</li>
-            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm'>Home Decor</li>
+        <ul className='flex  font-thin self-baseline-last '>
+            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm' onClick={handleLinkClick}>Home</li>    
+            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm' onClick={handleLinkClick}>Electronics</li>
+            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm' onClick={handleLinkClick}>Clothes</li>
+            <li className='cursor-pointer hover:border-1 hover:bg-pink-500 hover:border-[#1a1a2e] hover:text-[#1a1a2e] p-2 rounded-sm' onClick={handleLinkClick}>Home Decor</li>
         </ul>
         
         <button 
