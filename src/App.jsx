22ec09bbc78741271daaf915753345d09fe37fd6
Utkarsh <utkarshchoudhary405@gmail.com>
2025-04-20 17:40:31 +0530

    implemented product saga and home page, imported a custom card to show product details

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchProductsRequest } from './store/Slice/productSlice'
 
 
 
 function App() {
-  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProductsRequest())  // triggers saga
+  }, [dispatch])
 
   return (
     <div>
--- src/store/saga/productSaga.js
+++ src/store/saga/productSaga.js
@@ -1,3 +1,20 @@
-export default function* productSaga(){
+import {call, put, takeLatest} from 'redux-saga/effects'
 
+import { fetchProductFailure, fetchProductSuccess, fetchProductsRequest } from '../Slice/productSlice'
+
+function fetchProductsApi(){
+    return fetch('/products.json').then((res)=>res.json())
+}
+
+function* fetchProducts() {
+    try {
+        const products = yield call(fetchProductsApi) 
+        yield put(fetchProductSuccess(products)) 
+    } catch (error) {
+    yield put(fetchProductFailure(error.message)) 
+    }
+}
+
+export default function* productSaga(){
+    yield takeLatest(fetchProductsRequest.type, fetchProducts)
 }
